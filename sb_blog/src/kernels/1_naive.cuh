8caeda9a44e0b97697408fb5658b36229191dca3
yeti-teti <yetit1762@gmail.com>
2025-01-25 16:09:36 -0800

    k1_blog

---
+++ sb_blog/src/kernels/1_naive.cuh
@@ -0,0 +1,40 @@
+// Kernel: Naive, GFLOPs/s: 309.0 ,  Performance relative to cuBLAS: 1.3% 
+
+#include<stdio.h>
+#incldue<stdlib.h>
+#include<cublas_v2.h>
+#include<cuda_runtime.h>
+
+/*
+Matrix sizes:
+MxK * KxN = MxN
+*/
+
+
+// The kernel will return different values based on the thread that's accessing them. 
+// Paremeters: (M: No. of rows in mat A, N: No. of Cols in Mat B, K: No. of cols in Mat A and No. of rows in Mat B, alpha, pointer to input Mat A, pointer to input Mat B, beta, pointer to output Mat C)
+__global__ void sgemm_naive(int M, int N, int K, int alpha, float* A, float* B, int beta, float* C){
+
+    // Global index to access elements of the arrays
+    const uint x = blockIdx.x * blockDim.x + threadIdx.x;
+    const uint y = blockIdx.y * blockDim.y + threadIdx.y;
+
+    // 'if' condition to check when M or N are not multiples of 32
+    // if statement is necessary to make things work under tile quantization
+    if(x < M && y < N){
+        float tmp = 0.0;
+
+        for(int i=0;i<K;i++){
+            tmp += A[x * K + i] * B[i * N + y];
+        }
+
+        // Compute position in C that this thread is responsible for
+        // C = α*(A@B)+β*C
+        C[x * N + y] = alpha * tmp + beta * C[x * N + y];
+    } 
+}
+
+
+
+
+
---
+++ sb_blog/src/runner.cu
@@ -0,0 +1,7 @@
+
+
+
+// Launching the first kernel
+// Assign each threat a unique entry in the result matrix C. Each thread will compute the dot product of the corresponding row of A and column of B.
+// The result will be written in C.
+// Each location of 
